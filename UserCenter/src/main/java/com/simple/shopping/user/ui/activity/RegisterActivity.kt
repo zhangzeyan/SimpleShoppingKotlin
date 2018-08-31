07bbf05c935d1b3ebf3300d3c178a07bbcdc6642
SimpleZhang38 <v-simzhang@expedia.com>
2018-08-31 17:57:24 +0800

    init baselibrary , provider, usercenter

---
+++ UserCenter/src/main/java/com/simple/shopping/user/ui/activity/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.simple.shopping.user.ui.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.simple.shopping.user.R
+
+class RegisterActivity: AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_register)
+    }
+}
